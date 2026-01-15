0be26b0214e72607a44bc9fb078621125fdfab1b
Chun Pan Yung <31672021+ChunPanYung@users.noreply.github.com>
2026-01-14 20:17:37 -0500

    feat: export function

--- roles/extra/files/init.bash
+++ roles/extra/files/init.bash
@@ -12,6 +12,7 @@ function rc_all_bash {
 	fi
 	unset rc
 }
+export -f rc_all_bash
 
 # Append to '$PATH' if directory exists and current in not '$PATH'
 function bash_add_path {
@@ -22,6 +23,7 @@ function bash_add_path {
 	export PATH
 	unset bin_path
 }
+export -f bash_add_path
 
 BASHRC_D=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
